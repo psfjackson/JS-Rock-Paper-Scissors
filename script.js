49deb84309ecd6f57d9ce1ad717a64c6fc873656
Phillip Jackson <phillip.s.f.jackson@gmail.com>
2019-06-13 23:10:22 +0100

    cleaned up code

--- script.js
+++ script.js
@@ -1,38 +1,21 @@
-  
-/* const addTextNode = function(text) {
-  let newtext = document.createTextNode(text);
-  let p1 = document.getElementById("p1");
-  p1.appendChild(newtext);
-}
-
-  // create a new div element 
-  let newDiv = document.createElement("div"); 
-  // and give it some content 
-  let newContent = document.createTextNode("Hi there and greetings!"); 
-  // add the text node to the newly created div
-  newDiv.appendChild(newContent);
-*/
-
-
-
 function addElement (msg) { 
-// Create a new, plain <span> element
-let sp1 = document.createElement("p");
+    // Create a new, plain <span> element
+    let sp1 = document.createElement("p");
 
-sp1.classList.add("consoleReview");
+    sp1.classList.add("consoleReview");
 
-// and give it some content 
-let newContent = document.createTextNode(msg); 
-// add the text node to the newly created div
-sp1.appendChild(newContent); 
+    // and give it some content 
+    let newContent = document.createTextNode(msg); 
+    // add the text node to the newly created div
+    sp1.appendChild(newContent); 
 
-// Get a reference to the element, before we want to insert the element
-let sp2 = document.getElementById("childElement");
-// Get a reference to the parent element
-let parentDiv = sp2.parentNode;
+    // Get a reference to the element, before we want to insert the element
+    let sp2 = document.getElementById("childElement");
+    // Get a reference to the parent element
+    let parentDiv = sp2.parentNode;
 
-// Insert the new element into the DOM before sp2
-parentDiv.insertBefore(sp1, sp2); 
+    // Insert the new element into the DOM before sp2
+    parentDiv.insertBefore(sp1, sp2); 
 }
 
 const computerPlay = function() {
@@ -47,8 +30,6 @@ const computerPlay = function() {
     }
     }
     
-  //Testing 'getComputerChoice
-  //console.log(computerPlay()); 
   
   const playRound = (playerSelection, computerSelection) =>{
     if (playerSelection === computerSelection) {
